0b2b80ad4e42cde217e9e26a3955e8665a4e6ef5
AncaSC <anca@streamcomputing.eu>
2014-11-04 17:00:16 +0200

    Added printf warning about kernel fastgen

--- src/gromacs/mdlib/forcerec.c
+++ src/gromacs/mdlib/forcerec.c
@@ -1808,7 +1808,9 @@ static void pick_nbnxn_resources(const t_commrec     *cr,
     // which means eeltype can change. The current implementation does not support
     // that yet.
     //bOclDoFastGen        = (getenv("OCL_NOFASTGEN") == NULL);
-    printf("\nWarning - all kernel flavours will be generated.\n\tFastGen temporary disabled. OCL_NOFASTGEN env var ignored.");
+#ifdef GMX_USE_OPENCL
+    printf("\nWarning - all kernel flavours will be generated.\n\tFastGen temporary disabled. OCL_NOFASTGEN env var ignored.\n");
+#endif
 
     bEmulateGPUEnvVarSet = (getenv("GMX_EMULATE_GPU") != NULL);
 
